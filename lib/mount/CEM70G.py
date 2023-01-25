f0d35c0738b10215fafe2973a34f651f862f511d
Damien Lasserre <damien.lasserre@gmail.com>
2023-01-26 00:03:44 +0100

    fixe bug

--- lib/mount/CEM70G.py
+++ lib/mount/CEM70G.py
@@ -76,11 +76,11 @@ class CEM70G:
         return CoordinateStructure(position[0:8], position[-9:])
 
     def move_to(self, ra, dec) -> None:
-        self.__send(':sRA'+ra+'#')
-        self.__send(':Sds'+dec+'#')
+        self.__send(':sRA' + ra + '#')
+        self.__send(':Sds' + dec + '#')
 
     @staticmethod
-    def _convert_arcs(arcs : int):
+    def _convert_arcs(arcs: int):
         degree = (arcs * 0.01) / 3600
         min = (degree % 1) * 60
         sec = round((((degree % 1) * 60) % 1) * 60, 2)
@@ -88,14 +88,18 @@ class CEM70G:
         return degree, min, sec
 
     def current_scope_position(self):
-        position = self.__send(':GEP#')
+        position = self.__send(':GAC#')
         sign = position[0:1]
 
-        dec_degree, dec_min, dec_sec = self._convert_arcs(int(position[1:9]))
-        ra_degree, ra_min, ra_sec = self._convert_arcs(int(position[9:18]))
+        _dec = int(position[1:9])
+        _ra = int(position[9:18])
+        if (_dec > 32400000 or _dec < -32400000) or (_ra > 129600000 or _ra < 0):  # retry call, bad data
+            return self.current_scope_position()
+        dec_degree, dec_min, dec_sec = self._convert_arcs(_dec)
+        ra_degree, ra_min, ra_sec = self._convert_arcs(_ra)
 
-        dec = 'ALT='+(sign+str(int(dec_degree))+'. '+str(int(dec_min))+"' "+str(round(dec_sec, 2))+'"')
-        ra = 'AZ='+(str(int(ra_degree))+'. '+str(int(ra_min))+"' "+str(round(ra_sec, 2))+'"')
+        dec = 'ALT=' + (sign + str(int(dec_degree)) + '. ' + str(int(dec_min)) + "' " + str(round(dec_sec, 2)) + '"')
+        ra = 'AZ=' + (str(int(ra_degree)) + '. ' + str(int(ra_min)) + "' " + str(round(ra_sec, 2)) + '"')
 
         return dec, ra
 
